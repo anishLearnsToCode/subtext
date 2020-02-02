6689bb241e5c859fad18f7150ad115ac160458e5
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-02-02 16:54:25 +1300

    adds current user whereabouts and also indicator to show that the convertation hasnt been started yet

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -14,26 +14,13 @@ export class ChatService {
 
   constructor() {
     this.mathematicsMessages.push(
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN),
-      new Message('anish', 'maths', '__bold__', MessageType.MARKDOWN)
     );
 
     this.physicsGroupMessages.push(
-      new Message('piyush', 'physics', "c = \\pm\\sqrt{a^2 + b^2}", MessageType.LATEX),
-      new Message('piyush', 'physics', '\\sum_{i=1}^nx_i', MessageType.LATEX),
-      new Message('piyush', 'physics', 'x^2 + 5', MessageType.LATEX),
-      new Message('piyush', 'physics', '\\sum_{i=1}^n(x_i^2 - \\overline{x}^2)', MessageType.LATEX),
+      new Message('piyush', 'physics', "$c = \\pm\\sqrt{a^2 + b^2}$", MessageType.LATEX),
+      new Message('piyush', 'physics', '$\\sum_{i=1}^nx_i$', MessageType.LATEX),
+      new Message('piyush', 'physics', '$x^2 + 5$', MessageType.LATEX),
+      new Message('piyush', 'physics', '$\\sum_{i=1}^n(x_i^2 - \\overline{x}^2)$', MessageType.LATEX),
     );
   }
 
