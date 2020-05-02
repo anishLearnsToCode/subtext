2512cce4d57af1fefc9b8e932807355eb4592ccc
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-03 01:40:51 +0530

    adds imports for firebase in app modul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { LoginComponent } from './login-page/login/login.component';
 import {AppRoutingModule} from "./app-routing/app-routing.module";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TestComponent } from './test/test.component';
+import {AngularFireModule } from 'angularfire2';
+import {environment} from "../environments/environment";
+import {AngularFireDatabaseModule} from 'angularfire2/database'
 
 @NgModule({
     declarations: [
@@ -22,7 +25,9 @@ import { TestComponent } from './test/test.component';
     BrowserModule,
     KatexModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
