99bdc9b99e95e7562b268b293b013013dec0df42
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-02 15:58:34 +0530

    removes redundant code from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,9 @@
 import { Component } from '@angular/core';
-import {GroupService} from './services/group.service';
-import {ChatService} from './services/chat.service';
-import {UserService} from "./services/user.service";
-import {MarkdownParserService} from "./services/markdown-parser.service";
-import {ClipboardService} from "./services/clipboard.service";
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'subtext';
-  message = '';
-
-  constructor(private readonly groupService: GroupService,
-              private readonly chatService: ChatService,
-              private readonly userService: UserService,
-              private readonly markdownParserService: MarkdownParserService) {
-  }
-
-  private updateSelectedGroup(groupName: string) {
-    this.groupService.currentlySelected = groupName;
-  }
-
-  isLatexMessage(): boolean {
-    return this.groupService.currentlySelected === 'maths' || this.groupService.currentlySelected === 'physics';
-  }
-
-  isMarkdownMessage(): boolean {
-    return this.groupService.currentlySelected === 'cs';
-  }
-
-  getMarkdownMessage(): string {
-    return MarkdownParserService.parse(this.message);
-  }
-
-  sendMessage() {
-    this.chatService.sendNewMessage(this.message, this.groupService.currentlySelected);
-    this.message = '';
-  }
-
-  copyMessageFromClipboard(): void {
-    this.message += ClipboardService.getMessage();
-  }
 }
