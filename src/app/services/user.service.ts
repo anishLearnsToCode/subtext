6d4c1e82c323caf1c116f57502b26a5915abed5f
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-03 03:40:45 +0530

    removes unnused import

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import {User} from "./user";
 import {AngularFireDatabase} from 'angularfire2/database';
 
 @Injectable({
@@ -10,7 +9,6 @@ export class UserService {
   private users = new Map<string, string>();
   private users$;
   private activeUser: string;
-  private activeUserKey: string;
 
   constructor(private readonly database: AngularFireDatabase) {
     this.users$ = database.list('/user');
@@ -18,18 +16,11 @@ export class UserService {
   }
 
   subscribeToChanges(): void {
-    // this.users$.valueChanges().subscribe(users => {
-    //   this.userNames = new Set<string>(users);
-    //   console.log(this.userNames);
-    // });
-
     this.users$.snapshotChanges().subscribe(users => {
+      this.users.clear();
       users.map(user => {
-        console.log(user.payload.node_.value_);
-        console.log(user.key);
         this.users.set(user.payload.node_.value_, user.key);
       });
-      console.log(this.users);
     });
   }
 
@@ -48,6 +39,5 @@ export class UserService {
 
   logout(): void {
     this.database.object('/user/' + this.users.get(this.activeUser)).remove();
-    this.users.delete(name);
   }
 }
