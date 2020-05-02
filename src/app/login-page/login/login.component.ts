1fc709168c6fd25802b1e95d136c0cf786bcd1e8
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-02 18:24:34 +0530

    adds login page and redirection functionality

--- src/app/login-page/login/login.component.ts
+++ src/app/login-page/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userName = '';
+  constructor(private readonly userService: UserService,
+              private readonly router: Router) { }
 
   ngOnInit() {
   }
 
+  userNameExists(): boolean {
+    return this.userService.userNameExist(this.userName);
+  }
+
+  joinChatRoom() {
+    this.userService.addUser(this.userName);
+    console.log(this.userName + ' added');
+    this.router.navigate(['dashboard']);
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from '@angular/core';
 import {User} from "./user";
-import {animate} from "@angular/animations";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  static anish: User = new User('anish');
-  private user = new User('anish');
+  private users: Set<User> = new Set<User>();
+  private userNames: Set<string> = new Set<string>();
+  private user;
 
   constructor() {
     this.user = new User('anish');
   }
 
-  static currentUser(): User {
-    return this.anish;
-  }
-
   public currentUser(): User {
     return this.user;
+  }
+
+  userNameExist(name: string): boolean {
+    return this.userNames.has(name);
+  }
+
+  addUser(name: string) {
+    const user = new User(name);
+    this.users.add(user);
+    this.userNames.add(name);
+    this.user = user;
   }
 }
