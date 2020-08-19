ab5768a42d46d4eca9b0b89ecdf6779a242ebdb3
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-08-19 18:55:50 +0530

    fixes tslint quote marks error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {MessageComponent} from './message/message.component';
-import {KatexModule} from "ng-katex";
-import {FormsModule} from "@angular/forms";
+import {KatexModule} from 'ng-katex';
+import {FormsModule} from '@angular/forms';
 import { LoginComponent } from './login-page/login/login.component';
-import {AppRoutingModule} from "./app-routing/app-routing.module";
+import {AppRoutingModule} from './app-routing/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TestComponent } from './test/test.component';
 import {AngularFireModule } from 'angularfire2';
-import {environment} from "../environments/environment";
-import {AngularFireDatabaseModule} from 'angularfire2/database'
+import {environment} from '../environments/environment';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
 
 @NgModule({
     declarations: [
